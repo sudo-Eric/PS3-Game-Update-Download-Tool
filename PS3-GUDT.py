fb8028b69d9c6497b512d1baaf5dbc57fca59045
sudo-Eric <ericheinke0@gmail.com>
2022-03-05 20:51:45 -0800

    Added log file

--- PS3-GUDT.py
+++ PS3-GUDT.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 import os
 import argparse
+import traceback
 import urllib.request
 import urllib3
 import xmltodict
 import hashlib
 import yaml
+import datetime
 
-version = "PS3-Game-Update-Download-Tool v1.0"
+version = "PS3-Game-Update-Download-Tool v1.1"
 
 PS3GameUpdateDataURL = "https://a0.ww.np.dl.playstation.net/tpl/np/%s/%s-ver.xml"
+
+logFile = None
 
 
 def getxml(url):
@@ -67,14 +71,24 @@ def downloadPackage(package, downloadFolder, overwriteExistingFiles):
     packageName = package["@url"].split("/")[-1]
     print(" * Downloading version %s: \"%s\" (%s)" %
           (package["@version"], packageURL, formatByteSize(package["@size"])))
-
+    logFile.write("Beginning download of %s (%s)\n" % (packageName, formatByteSize(package["@size"])))
+    # If download folder does not exist, create it
     if not os.path.exists(downloadFolder):
+        logFile.write(" * Creating directory \"%s\"" % downloadFolder)
         os.makedirs(downloadFolder)
+    # If file already exists and file overwriting is disabled, skip it
     if os.path.exists(downloadFolder + os.path.sep + packageName) and not overwriteExistingFiles:
         print("\t+File already exists. Skipping.")
+        logFile.write(" * File already exists. Skipping.\n")
         return
+    logFile.write(" * Version %s: %s\n" % (package["@version"], packageURL))
+    # Make sure log file is written before starting a download
+    logFile.flush()
+    os.fsync(logFile.fileno())
+    # Download file
     urllib.request.urlretrieve(packageURL, downloadFolder + os.path.sep + packageName)
 
+    # File hashes appear to always be different. Unsure why.
     # print("\t+Comparing file hash...", end="")
     # print("Pass") if sha1sumChecker(downloadFolder + os.path.sep + packageName, package["@sha1sum"]) else print("Fail")
 
@@ -84,16 +98,23 @@ def main(game_ID, downloadFolder, overwriteExistingFiles):
         downloadFolder += os.path.sep
     game_ID = game_ID.upper()
     print("Retrieving update data for \"%s\" from \"%s\"" % (game_ID, PS3GameUpdateDataURL % (game_ID, game_ID)))
+    logFile.write("Retrieving update data for \"%s\" from \"%s\"\n" % (game_ID, PS3GameUpdateDataURL % (game_ID, game_ID)))
     xml = getxml(PS3GameUpdateDataURL % (game_ID, game_ID))
+    logFile.write("Data retrieved: ")
+    # If the XML file does not exist, no updates are available
     if xml is None:
         print("There are no updates available for this game")
+        logFile.write("There are no updates available for this game\n")
         return
+    # If there is only one update for a game
     if type(xml["titlepatch"]["tag"]["package"]) is list:
         gameName = removeIllegalFileNameCharacters(xml["titlepatch"]["tag"]["package"][-1]["paramsfo"]["TITLE"])
         print("Updates were found for \"%s\"" % gameName)
+        logFile.write("Updates were found for \"%s\"\n" % gameName)
         downloadFolder += ("%s [%s] Updates" % (gameName, game_ID))
         for package in xml["titlepatch"]["tag"]["package"]:
             downloadPackage(package, downloadFolder, overwriteExistingFiles)
+    # If there are multiple updates for a game
     else:
         gameName = removeIllegalFileNameCharacters(xml["titlepatch"]["tag"]["package"]["paramsfo"]["TITLE"])
         print("Updates were found for \"%s\"" % gameName)
@@ -117,10 +138,21 @@ if __name__ == '__main__':
                         help="Overwrite existing files")
     args = parser.parse_args()
 
+    # Set up log file
+    logFile = open('log.txt', 'a')
+    logFile.write("Script (%s) started at %s (Local %s)\n" %
+                  (version, datetime.datetime.utcnow().isoformat(), datetime.datetime.now().isoformat()))
+    logFile.write("Inputs: game_id %s\tgame_list %s\tdest %s\toverwrite %s\n" %
+                  (args.gameID, args.gameList, args.downloadFolder, args.overwrite))
+
+    # Download a specific games updates
     if args.gameID is not None:
         main(args.gameID, args.downloadFolder, args.overwrite)
+    # Download updates for multiple games at a time
     else:
         with open(args.gameList) as f:
             gameIDs = yaml.load(f, Loader=yaml.loader.SafeLoader)
+        logFile.write("Loaded games list from %s\n" % args.gameList)
         for ID in gameIDs:
             main(ID, args.downloadFolder, args.overwrite)
+    logFile.close()
